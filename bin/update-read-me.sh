227219bc6ff37b2f5f589ec8a21c20584038e0d2
Havimaki <dm.havimaki@gmail.com>
2020-08-01 23:37:01 -0400

    Get file and directory dynamically

--- bin/update-read-me.sh
+++ bin/update-read-me.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 . ./bin/list-directories.sh --source-only
-list_directories
+# list_directories
 
-echo what is the filename?; read file
+diff="$(git diff --name-only . ':(exclude)bin/update-read-me.sh')"
+directory="$(echo "${diff%%/*}")"
+file="$(echo "$diff" | cut -d'/' -f 2)"
 
+cd $directory
 mapfile < $file
 
 # update READ ME
